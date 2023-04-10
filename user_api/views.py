2dd33ea3446dfb22f09989073da19e87c2e69490
sandeep <sandeepbisht045>
2023-04-11 00:16:38 +0530

    changed some json response

--- user_api/views.py
+++ user_api/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from .settings import EMAIL_ID
 
 def data_get(request):
+        code = request.GET.get('code') or ''
         # Service Account Credential file
         SERVICE_ACCOUNT_FILE = 'cred.json'
         # Google Api Scopes for entire function
@@ -14,20 +16,23 @@ def data_get(request):
         creds = service_account.Credentials.from_service_account_file(
                 SERVICE_ACCOUNT_FILE, scopes=scopes)
 
-        emailId = "ajay.singh@freecharge.com"
+        emailId = EMAIL_ID
 
         # Making Domain wide delegation for the user/email id
         delegated_credentials = creds.with_subject(emailId)
        
         # Creating Drive service using Domain wide delegated credentials
         directory_service = build('admin', 'directory_v1', credentials=delegated_credentials)
-        #sT = datetime.utcnow() # <-- get time in UTC
-        #starttime = sT.isoformat("T") + "Z"  
-        # startTime='2023-02-18T10:26:35.000Z'      
-        # Making multiple Api call to re
         results = directory_service.users().list(domain= 'freecharge.com', maxResults=500
                                 ,orderBy='email').execute()
         users = results.get('users', [])
-        # print(len(users))
-        users = [{'id':data.get('id'),'fullName':data.get('name').get('fullName'),'primaryEmail':data.get('primaryEmail'),'suspended':data.get('suspended')} for data in users]
-        return JsonResponse(users,safe=False)
+        # data = [{'employeeCode':data['externalIds'][0]['value'],'primaryEmail':data.get('primaryEmail')
+        #           ,'suspended':data.get('suspended')} for data in users if data['externalIds'][0]['value'] == code]
+        data = []
+        for user in users:
+                if  user['externalIds'][0]['value'] == code:
+                        matched_user_data = {'employeeCode':user['externalIds'][0]['value'],'primaryEmail':user.get('primaryEmail')
+                  ,'suspended':user.get('suspended')}
+                        data.append(matched_user_data)
+                        break
+        return JsonResponse(data,safe=False)
